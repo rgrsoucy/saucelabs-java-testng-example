8a7ef08f2ab50c4c16c4d3a93067e055ea042e66
sauceaaron <aaron.evans@saucelabs.com>
2018-03-14 09:49:31 -0600

    create POJO for converting SAUCE_ONDEMAND_BROWSERS json to object

---
+++ src/main/java/TestSettings.java
@@ -0,0 +1,57 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+
+import java.util.List;
+
+public class TestSettings
+{
+	@SerializedName("os")
+	public String os;
+
+	@SerializedName("platform")
+	public String platformName;
+
+	@SerializedName("browser")
+	public String browserName;
+
+	@SerializedName("browser-version")
+	public String browserVersion;
+
+	@SerializedName("long-name")
+	public String browserNameLong;
+
+	@SerializedName("long-version")
+	public String browserVersionLong;
+
+	@SerializedName("url")
+	public String url;
+
+	public static Gson parser = new GsonBuilder().disableHtmlEscaping().create();
+
+	public static List<TestSettings> parseMultiCapabilities(String json)
+	{
+		Type TestSettingsListType = new TypeToken<List<TestSettings>>(){}.getType();
+
+		return parser.fromJson(json, TestSettingsListType);
+	}
+
+	public TestSettings fromJson(String json)
+	{
+		Type TestSettingsType = new TypeToken<List<TestSettings>>(){}.getType();
+		return parser.fromJson(json, TestSettingsType);
+	}
+
+	public String toJson()
+	{
+		return parser.toJson(this);
+	}
+
+	public String toString()
+	{
+		return toJson();
+	}
+}
